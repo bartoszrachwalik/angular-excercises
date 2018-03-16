48d00bb50ba24f3a971ab2154e05ae99be8c84c4
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-03-16 15:29:32 +0100

    adds alerts, warning and success components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import {ServerComponent} from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import {ServersComponent} from './servers/servers.component';
+import {WarningComponent} from './alerts/warning/warning.component';
+import {SuccessComponent} from './alerts/success/success.component';
+import {AlertsComponent} from './alerts/alerts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningComponent,
+    SuccessComponent,
+    AlertsComponent
   ],
   imports: [
     BrowserModule,
