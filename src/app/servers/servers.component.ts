ca9909f09c973619c4f85f45a6d0fa95997908e1
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-03-20 07:54:06 +0100

    more event binding example and two way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ import {Component, OnInit} from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
 
   constructor() {
     setTimeout(() => {
@@ -26,7 +27,12 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 
+
   onCreateServer() {
     this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServername(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
