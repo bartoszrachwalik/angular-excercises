7b338462c3728b62ceb3e4adcfb382b9605abb44
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-03-20 08:57:17 +0100

    structural directive example

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ import {Component, OnInit} from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverCreated = false;
   serverName = 'Testserver';
   userName = '';
 
@@ -29,6 +30,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
