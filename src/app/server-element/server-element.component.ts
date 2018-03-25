f41e4f1b44cb115974202d0b4b6516ece6fa4d35
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-03-25 23:04:24 +0200

    adds cockpit and server element components

---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+  // allows to use that property by other components
+  @Input() element: {
+    type: string,
+    name: string,
+    content: string
+  };
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
