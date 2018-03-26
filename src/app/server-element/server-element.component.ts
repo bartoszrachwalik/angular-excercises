8a3c0602d7024a75495a9a0717651c03be8b3571
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-03-26 16:41:59 +0200

    overwriting view encapsulation example

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // it makes this component's style global
+  // encapsulation: ViewEncapsulation.None
 })
 export class ServerElementComponent implements OnInit {
   // allows to use that property by other components
