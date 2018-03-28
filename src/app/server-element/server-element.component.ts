e0f14179c0a51061759554e3d244736a61dbc02d
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-03-28 23:43:28 +0200

    clears unnecessary code

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,17 +1,9 @@
 import {
-  AfterContentChecked,
-  AfterContentInit,
-  AfterViewChecked,
-  AfterViewInit,
   Component,
   ContentChild,
-  DoCheck,
   ElementRef,
   Input,
-  OnChanges,
-  OnDestroy,
   OnInit,
-  SimpleChanges,
   ViewChild,
   ViewEncapsulation
 } from '@angular/core';
@@ -23,14 +15,7 @@ import {
   // it makes this component's style global
   // encapsulation: ViewEncapsulation.None
 })
-export class ServerElementComponent implements OnInit,
-  OnChanges,
-  DoCheck,
-  AfterContentInit,
-  AfterContentChecked,
-  AfterViewInit,
-  AfterViewChecked,
-  OnDestroy {
+export class ServerElementComponent implements OnInit {
   // allows to use that property by other components
   // you can use aliases; example: @Input('srvElement')
   @Input() element: {
@@ -47,47 +32,8 @@ export class ServerElementComponent implements OnInit,
   @ContentChild('contentParagraph') paragraph: ElementRef;
 
   constructor() {
-    console.log('constructor called');
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    console.log('ngOnChanges called');
-    console.log(changes);
-  }
-
-  // won't log header, too early in the lifecycle
   ngOnInit() {
-    console.log('ngOnInit called');
-    console.log('Text content: ' + this.header.nativeElement.textContent);
-    console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent);
-  }
-
-  ngDoCheck() {
-    console.log('ngDoCheck called');
-  }
-
-  // it gives access to content
-  ngAfterContentInit() {
-    console.log('ngAfterContentInit called');
-    console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent);
-  }
-
-  ngAfterContentChecked() {
-    console.log('ngAfterContentChecked called');
-
-  }
-
-  // afterViewInit gives access to template elements, before you reach it you can't see values
-  ngAfterViewInit() {
-    console.log('ngAfterViewInit called');
-    console.log('Text content: ' + this.header.nativeElement.textContent);
-  }
-
-  ngAfterViewChecked() {
-    console.log('ngAfterViewChecked called');
-  }
-
-  ngOnDestroy() {
-    console.log('ngOnDestroy called');
   }
 }
