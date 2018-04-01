4ed99d794ccf068327d3db7d63289a7bfdfaf4a5
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-04-01 18:46:54 +0200

    creates a basic attribute directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import {ServersComponent} from './servers/servers.component';
 import {WarningComponent} from './alerts/warning/warning.component';
 import {SuccessComponent} from './alerts/success/success.component';
 import {AlertsComponent} from './alerts/alerts.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
-import { GameControlComponent } from './game-control/game-control.component';
-import { OddComponent } from './game-control/odd/odd.component';
-import { EvenComponent } from './game-control/even/even.component';
+import {CockpitComponent} from './cockpit/cockpit.component';
+import {ServerElementComponent} from './server-element/server-element.component';
+import {GameControlComponent} from './game-control/game-control.component';
+import {OddComponent} from './game-control/odd/odd.component';
+import {EvenComponent} from './game-control/even/even.component';
+import {BasicHighlightDirective} from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { EvenComponent } from './game-control/even/even.component';
     ServerElementComponent,
     OddComponent,
     EvenComponent,
-    GameControlComponent
+    GameControlComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule,
