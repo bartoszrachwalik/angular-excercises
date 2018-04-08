854f144d9efba88ff7b95449030d78947cbaf496
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-04-08 11:22:07 +0200

    binding to directive properties

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,15 +1,20 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @Input() defaultColor: string = 'transparent';
+  // you can set an alias equal to selector, app.component html line 88
+  @Input() highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
   }
 
   ngOnInit() {
+    // we have values available here before template is rendered so we dont't have a bug when initially background is transparent
+    this.backgroundColor = this.defaultColor;
     // better approach than basic directive because you might not have direct access to DOM and the native element and it style
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
@@ -18,13 +23,13 @@ export class BetterHighlightDirective implements OnInit {
   @HostListener('mouseenter')
   mouseover(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave')
   mouseleave(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
 }
